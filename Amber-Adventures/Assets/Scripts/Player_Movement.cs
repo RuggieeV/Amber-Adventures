ef10d573244ba76d1c4ac76577ce19dd6a39b946
s232926 <168423755+s232926@users.noreply.github.com>
2024-09-11 20:22:55 +0930

    Added Lucario Dash, started AfterImages
    Scripted the dash, it works
    Temporary particle trail for dash
    Started AfterImages, problem calling PlayerCharacter. AfterImages may be behind layers. AfterImages are in new "Player" layer

--- Amber-Adventures/Assets/Scripts/Player_Movement.cs
+++ Amber-Adventures/Assets/Scripts/Player_Movement.cs
@@ -1,39 +1,62 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using UnityEngine.Animations;
 
 public class Player_Movement : MonoBehaviour
 {
-    Rigidbody2D body;
-
+    Rigidbody2D rb;
     Animator m_anim;
-
     float horizontal;
     float vertical;
-
     public float runSpeed = 3.0f;
+
+    private bool canDash = true;
+    private bool isDashing;
+    public float dashingPower;
+    public float dashingTime;
+    public float dashingCooldown;
+
+    //[SerializeField] private TrailRenderer trail; //temporary effect
+    public float distanceBetweenImages;
+    private float lastImageXpos;
 
     void Start()
     {
-        body = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
         m_anim = GetComponentInChildren<Animator>();
     }
 
     void Update()
     {
+        if (isDashing)
+        {
+            return;
+        } //'pauses' movement until dash is finished
+
         // Gives a value between -1 and 1
         horizontal = Input.GetAxisRaw("Horizontal"); // -1 is left
         vertical = Input.GetAxisRaw("Vertical"); // -1 is down
-        Animation();
+        MovementAnimation();
+
+        if (Input.GetKeyDown(KeyCode.Space) && canDash)
+        {
+            StartCoroutine(Dash());
+        } //if player canDash, active coroutine(Dash)
     }
 
     void FixedUpdate()
     {
-        body.velocity = new Vector2(horizontal, vertical).normalized * runSpeed;
+        if (isDashing)
+        {
+            return;
+        } //'pauses' movement until dash is finished
+        rb.velocity = new Vector2(horizontal, vertical).normalized * runSpeed;
     }
 
-    void Animation()
+    void MovementAnimation()
     {
         m_anim.speed = 1;
         if (vertical > 0)
@@ -62,4 +85,24 @@ public class Player_Movement : MonoBehaviour
         }
     }
 
+    private IEnumerator Dash()
+    {
+        canDash = false;
+        isDashing = true;
+        // rb.velocity = new Vector2(transform.localScale.x * dashingPower, 0f);
+        rb.velocity = new Vector2(horizontal, vertical).normalized * dashingPower;
+        //enable animation (afteraffects, aura, trails, etc)
+        if (Mathf.Abs(transform.position.x - lastImageXpos) > distanceBetweenImages)
+        {
+            PlayerAfterImagePool.Instance.GetFromPool();
+            lastImageXpos = transform.position.x;
+        }
+        //trail.emitting = true;
+        yield return new WaitForSeconds(dashingTime);
+        //trail.emitting = false;
+        isDashing = false;
+        //disable animation (afteraffects, aura, trails, etc)
+        yield return new WaitForSeconds(dashingCooldown);
+        canDash = true;
+    } //Activate dash and cooldown
 }
