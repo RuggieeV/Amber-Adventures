18d0613e875783e5becafdd7284987fb45fc7bbf
s232926 <168423755+s232926@users.noreply.github.com>
2024-11-06 18:02:17 +1030

    Build

---
+++ Amber-Adventures/Assets/BossDoor.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class BossDoor : MonoBehaviour
+{
+    public GameObject Absol;
+
+
+}
